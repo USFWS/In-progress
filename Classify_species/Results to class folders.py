363ba4e5de08d8c623dc65f93b6cd86bdba0ed09
Brad Pickens <bradley_pickens@fws.gov>
2026-03-10 08:45:41 -0400

    test

--- Classify_species/Results to class folders.py
+++ Classify_species/Results to class folders.py
@@ -3,6 +3,8 @@ import pandas
 from os.path import basename
 import shutil
 
+## first attempt
+
 ## New inputs: drive_path = root directory, flight_name = flight folder, model_path = model to apply
 root_path = "D:/species_2025/"
 flight_name = "3_crops_all_inference"
